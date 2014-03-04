6454f0eb15b594d2a966b68f55cfd24369457328
Narayan Newton <nnewton@tag1consulting.com>
2014-03-04 14:55:58 -0800

    Update module to support RHEL

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,15 +19,16 @@ class denyhosts (
       enable    => true,
       hasstatus => false,
       pattern   => "denyhosts",
-      require   => Package["denyhosts"],
+      require   => [ Package["denyhosts"], File['/var/lib/denyhosts/allowed-hosts'], ]
   }
 
   file {
-    "/etc/hosts.allow":
-      content => inline_template('<%= [ allow.to_a.join("\n") , "\n" ].join %>'),
+    "/var/lib/denyhosts/allowed-hosts":
+      content => inline_template('<%= [ @allow.to_a.join("\n") , "\n" ].join %>'),
       owner   => root,
       group   => root,
       mode    => 644,
+      require => File['/etc/denyhosts.conf'],
   }
 
 }
